af3d3ae8ae1a504d5796b284e655e3944c263cb0
chintan-albiorix <chintand.albiorix@gmail.com>
2022-05-05 15:20:20 +0530

    rxjs code updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { ObservableComponent } from './observable/observable.component';
+import { PromiseComponent } from './promise/promise.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:PromiseComponent},
+  {path:'promise', component:PromiseComponent},
+  {path:'observable', component:ObservableComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PromiseComponent } from './promise/promise.component';
+import { ObservableComponent } from './observable/observable.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PromiseComponent,
+    ObservableComponent
   ],
   imports: [
     BrowserModule,
